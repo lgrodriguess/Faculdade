c0d96bbfc6195bbf4dc634952dccf105d1dd0d62
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-07 21:34:16 -0300

    Create Ex1S1.java

---
+++ Orientação a Objetos/exercicios/Semana1/Ex01/Ex1S1.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.ex1s1;
+
+public class Ex1S1 {
+
+    public static void main(String[] args) {
+            Roda r1 = new Roda(6, "ferro", 45, 400);
+            Produto p1 = new Produto("Cereal", 63, 18.9);
+    }
+}
