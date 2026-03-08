3bcc96f7950bb5b77803cf026af123e97ee6724c
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-07 23:24:14 -0300

    Create Carro.java

---
+++ Orientação a Objetos/exercicios/Semana1.1/Ex6/Carro.java
@@ -0,0 +1,9 @@
+//Faça um modelo para a classe Carro. Pense em cada atributo que este novo tipo deve ter, se necessário pode usar outro tipo abstrato recém criado
+package com.mycompany.ex6;
+
+public class Carro {
+        private String nome;
+        private String marca;
+        private int ano;
+        private double preco;
+}
