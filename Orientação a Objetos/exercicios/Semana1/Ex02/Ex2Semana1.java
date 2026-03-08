28ac44a2da4f3a32f92413580d07d2ba5036983d
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-07 22:32:02 -0300

    Update Ex2Semana1.java

--- Orientação a Objetos/exercicios/Semana1/Ex02/Ex2Semana1.java
+++ Orientação a Objetos/exercicios/Semana1/Ex02/Ex2Semana1.java
@@ -1,4 +1,4 @@
-
+aça uma classe para o modelo “Roda”. Instancie dois objetos para a classe carro e preencha os atributos dos objetos utilizando os métodos set
 package com.mycompany.ex2semana1;
 
 public class Ex2Semana1 {
