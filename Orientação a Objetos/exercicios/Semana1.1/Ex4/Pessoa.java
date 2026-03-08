c78c171a86166afd45161b308be6d9a8f2d6e269
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-07 23:20:12 -0300

    Create Pessoa.java

---
+++ Orientação a Objetos/exercicios/Semana1.1/Ex4/Pessoa.java
@@ -0,0 +1,12 @@
+//No processo de criação de software um passo importantíssimo é a modelagem. Neste momento pensamos nas características essenciais de nossos sistemas. Faça um modelo para a classe Pessoa. Pense em cada atributo que este novo tipo deve ter.
+package com.mycompany.ex4;
+
+public class Pessoa {
+        private String nome;
+        private int idade;
+        private double altura;
+        private String profissao;
+        private String endereco;
+        
+
+}
