62af88a97690fd83cdcc482e279676e6cc8bbbf1
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-07 23:22:17 -0300

    Create Roda.java

---
+++ Orientação a Objetos/exercicios/Semana1.1/Ex5/Roda.java
@@ -0,0 +1,9 @@
+//Faça um modelo para a classe Roda. Pense em cada atributo que este novo tipo deve ter.
+package com.mycompany.ex5;
+
+public class Roda {
+       private int raio;
+       private String material;
+       private double peso;
+       private double maxPeso;
+}
