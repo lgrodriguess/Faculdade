8c595d5975e45e57c24a2d2058bff371431afd4f
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 15:30:40 -0300

    Create Ex0.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex0/Ex0.java
@@ -0,0 +1,11 @@
+package com.mycompany.ex1;
+
+public class Ex0 {
+
+    public static void main(String[] args) {
+        Tv tv1 = new Tv();
+        
+        tv1.fill();
+        System.out.println(tv1.toString());
+    }
+}
