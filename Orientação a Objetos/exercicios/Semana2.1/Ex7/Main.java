fec2c60e04cef0c2f730242c3d6f3790334367c3
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 19:34:11 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex7/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex7;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Filme f1 = new Filme();
+        
+        f1.fill();
+        System.out.println(f1.toString());
+    }
+}
