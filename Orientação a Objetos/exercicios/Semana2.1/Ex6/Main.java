d06c8c1a6f8f38e42fcd5114c2de16993ae47cdd
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 19:30:59 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex6/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bicicleta b1 = new Bicicleta();
+        
+        b1.fill();
+        System.out.println(b1.toString());
+    }
+}
