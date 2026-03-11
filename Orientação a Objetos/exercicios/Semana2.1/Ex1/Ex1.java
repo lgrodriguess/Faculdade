0d9710f7809e65a9bed91e3aa47f077fd8aa3698
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 15:37:21 -0300

    Create Ex1.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex1/Ex1.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject2;
+
+public class Ex1{
+
+    public static void main(String[] args) {
+        TelefoneCelular celular = new TelefoneCelular();
+        
+        celular.fill();
+        System.out.println(celular.toString());
+      
+    }
+}
