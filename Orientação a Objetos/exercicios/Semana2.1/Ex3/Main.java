6f185aa563f16d29ad01f141c1714aa8c531be12
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 16:51:11 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex3/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        
+        a1.fill();
+        System.out.println(a1.toString());
+    }
+}
