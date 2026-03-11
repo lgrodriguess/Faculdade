0261448e0427d0210d31e55fc3bf6ecd08dfcb37
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 17:40:12 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex5/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnimalEst a1 = new AnimalEst();
+        
+        a1.fill();
+        System.out.println(a1.toString());
+    }
+}
