fbff858eda75d55da6f7d3c6d4edf4b7d4529bd3
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 16:18:11 -0300

    Create Ex2.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex2/Ex2.java
@@ -0,0 +1,12 @@
+package com.mycompany.ex2;
+
+public class Ex2 {
+
+    public static void main(String[] args) {
+        Casa c1 = new Casa();
+        
+        c1.fill();
+        System.out.println(c1.toString());
+        
+    }
+}
