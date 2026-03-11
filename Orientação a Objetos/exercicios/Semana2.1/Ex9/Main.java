181c24a1bc911fc81b65147e9a31f081043a7fb1
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 20:09:37 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex9/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JogoVideogame j1 = new JogoVideogame();
+        
+        j1.fill();
+        System.out.println(j1.toString());
+    }
+}
