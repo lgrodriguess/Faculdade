3ec7b3de0857023c224db2c341c3e48eabb290fd
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 18:08:05 -0300

    Update Main.java

--- Orientação a Objetos/exercicios/Semana2.1/Ex5/Main.java
+++ Orientação a Objetos/exercicios/Semana2.1/Ex5/Main.java
@@ -1,11 +1,24 @@
-package com.github.lgrodriguess.ex5;
+package com.github.lgrodriguess.ex5certo;
 
 public class Main {
 
     public static void main(String[] args) {
-        AnimalEst a1 = new AnimalEst();
+        AnimalEst a1= new AnimalEst();
+        AnimalEst a2= new AnimalEst();
+        AnimalEst a3= new AnimalEst();
         
         a1.fill();
+        a2.fill();
+        a3.fill();
+        
+        System.out.println("Animal 1:");
         System.out.println(a1.toString());
+        
+        System.out.println("Animal 2:");
+        System.out.println(a2.toString());
+        
+        System.out.println("Animal 3:");
+        System.out.println(a3.toString());
+        
     }
 }
