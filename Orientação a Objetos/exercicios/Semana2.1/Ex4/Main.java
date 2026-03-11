ebcd936e8055d1b521bccbe5d3ded25bce2fb9ed
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 17:19:18 -0300

    Create Main.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex4/Main.java
@@ -0,0 +1,11 @@
+package com.github.lgrodriguess.ex5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Livro l1 = new Livro();
+        
+        l1.fill();
+        System.out.println(l1.toString());
+    }
+}
