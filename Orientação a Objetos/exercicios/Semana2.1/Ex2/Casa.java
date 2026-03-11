771c50d585beb57cbfb8cbb533f418632516e398
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-11 16:21:03 -0300

    Create Casa.java

---
+++ Orientação a Objetos/exercicios/Semana2.1/Ex2/Casa.java
@@ -0,0 +1,83 @@
+
+package com.mycompany.ex2;
+import java.util.Scanner;
+public class Casa {
+    private String endereco;
+    private double area;
+    private int quartos;
+    private double preco;
+    
+    Casa(){
+        this.endereco = "";
+        this.area = 0.0;
+        this.quartos = 0;
+        this.preco = 0.0;
+    }
+    Casa(String endereco, double area, int quartos, double preco){
+        this.endereco = endereco;
+        this.area = area;
+        this.quartos = quartos;
+        this.preco = preco;
+    }
+    
+   //Copiar
+    Casa(Casa other){
+        this.endereco = other.getEndereco();
+        this.area = other.getArea();
+        this.quartos = other.getQuartos();
+        this.preco = other.getPreco();
+    }
+    
+    //Getters
+    private String getEndereco(){
+        return this.endereco;
+    }
+    private double getArea(){
+        return this.area;
+    }
+    private int getQuartos(){
+        return this.quartos;
+    }
+    private double getPreco(){
+        return this.preco;
+    }
+    
+    //Setters
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public void setArea(double area){
+        this.area = area;
+    }
+    public void setQuartos(int quartos){
+        this.quartos = quartos;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    
+    //Preencher
+    public void fill(){
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Endereco:");
+        this.endereco = ler.nextLine();
+        System.out.println("Area:");
+        this.area = ler.nextDouble(); ler.nextLine();
+        System.out.println("Quartos:");
+        this.quartos = ler.nextInt(); ler.nextLine();
+        System.out.println("Preco:");
+        this.preco = ler.nextDouble(); ler.nextLine();
+    }
+    
+    //Imprimir
+    @Override
+    public String toString(){
+        return "Casa{ \n" +
+                "Endereco='" + this.endereco + "'\n" +
+                "Area='" + this.area + "'\n" +
+                "Numero Quartos='" + this.quartos + "'\n" +
+                "Preco='" + this.preco +
+                "'}";
+    }
+}
