91c7003749bbde9e8fae64cbf354bcbf632bcc3c
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-29 18:40:48 -0300

    Add Livro class with properties and methods

---
+++ Orientação a Objetos/exercicios/Semana2.4/Ex02/Livro.java
@@ -0,0 +1,58 @@
+package com.github.viniplugner.ex02;
+
+import java.util.Scanner;
+
+public class Livro {
+
+    public static String libraryName = "Biblioteca Central";
+
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Livro() {}
+
+    public Livro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public int getPaginas() { return paginas; }
+    public void setPaginas(int paginas) { this.paginas = paginas; }
+
+    public void fill() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Título: ");
+        this.titulo = scanner.nextLine();
+
+        System.out.print("Autor: ");
+        this.autor = scanner.nextLine();
+
+        System.out.print("Páginas: ");
+        this.paginas = scanner.nextInt(); scanner.nextLine();
+    }
+
+    public void copy(Livro other) {
+        this.titulo = other.getTitulo();
+        this.autor = other.getAutor();
+        this.paginas = other.getPaginas();
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + this.titulo + '\'' +
+                ", autor='" + this.autor + '\'' +
+                ", paginas=" + this.paginas +
+                ", biblioteca='" + libraryName + '\'' +
+                '}';
+    }
+}
