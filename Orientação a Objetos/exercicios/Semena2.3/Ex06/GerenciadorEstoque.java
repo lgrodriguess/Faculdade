4cb4f145404a1b3d695f2100b2f40d1d7e760afa
Luan Rodrigues <lrodriges2407@gmail.com>
2026-03-29 18:36:57 -0300

    Implement GerenciadorEstoque class for product management

---
+++ Orientação a Objetos/exercicios/Semena2.3/Ex06/GerenciadorEstoque.java
@@ -0,0 +1,62 @@
+package com.github.lgrodriguess.mavenproject2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GerenciadorEstoque implements Iterable<Produto> { 
+
+    private final List<Produto> productList;
+
+    public GerenciadorEstoque() {
+        this.productList = new ArrayList<>();
+    }
+
+    public void addProduct(Produto produto) {
+        getProductList().add(produto);
+    }
+
+    
+    public boolean removeProduct(Produto produto) {
+        return getProductList().remove(produto);
+    }
+
+    public boolean updateQuantity(int code, int newQuantity) {
+        for (Produto produto : this.productList) {
+            if (produto.getCodigo() == code) {
+                produto.setQuantidade(newQuantity);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Produto findByCode(int code) {
+        for (Produto produto : getProductList()) {
+            if (produto.getCodigo() == code) {
+                return produto;
+            }
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return getProductList().isEmpty();
+    }
+
+    public void listProducts() {
+        for (Produto produto : getProductList()) {
+            System.out.println(produto);
+        }
+    }
+
+    public List<Produto> getProductList() {
+        return productList;
+    }
+
+  
+    @Override
+    public Iterator<Produto> iterator() {
+        return productList.iterator();
+    }
+}
